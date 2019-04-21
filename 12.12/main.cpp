4afd7687874b3f00dfbda1b439dfba2d21526af1
DESKTOP-30ETN09\黎炳政 <1074586195@qq.com>
2019-04-21 19:45:20 +0800

    BasePlusCommissionEmployee.cpp

--- 12.12/main.cpp
+++ 12.12/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-
+#include "BasePlusCommissionEmployee.h"
 using namespace std;
 
-int main()
+
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+   const string &first, const string &last, const string &ssn,
+   int month, int day, int year, double sales,
+   double rate, double salary )
+   : CommissionEmployee( first, last, ssn, month, day, year, sales, rate )
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   setBaseSalary( salary );
+}
+
+
+void BasePlusCommissionEmployee::setBaseSalary( double salary )
+{
+   baseSalary = ( ( salary < 0.0 ) ? 0.0 : salary );
+}
+
+
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+}
+
+
+double BasePlusCommissionEmployee::earnings() const
+{
+    return getBaseSalary() + CommissionEmployee::earnings();
+}
+
+void BasePlusCommissionEmployee::print() const
+{
+   cout << "base-salaried ";
+   CommissionEmployee::print();
+   cout << "; base salary: " << getBaseSalary();
 }
